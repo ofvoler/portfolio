2b72a95496607472a07f8e57c26ea091974d5c8b
ofvoler <ofvoler@gmail.com>
2021-03-28 20:46:25 -0400

    Fix prop import

--- src/components/button.js
+++ src/components/button.js
@@ -2,10 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 import "./button.css"
 
-export default function Button({ props }) {
+export default function Button(props) {
     return (
-        <Link to={props.link} className="buttonLink">
-            <div className="buttonStyle">{props.buttonText}</div>
+        <Link to={props.buttonLink}>
+            <div className="buttonLink">
+                <div className="buttonStyle">{props.buttonText}</div>
+            </div>
         </Link>
     )
 }
