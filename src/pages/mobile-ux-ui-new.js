374ad6cd8f955ad660f580e819ecb2f81b2ac73a
ofvoler <ofvoler@gmail.com>
2021-04-05 17:18:14 -0400

    Create .js page to test styling for portfolio page

---
+++ src/pages/mobile-ux-ui-new.js
@@ -0,0 +1,61 @@
+import React from "react"
+import Layout from "../components/layout"
+import "../css/styles.css"
+
+export default function MobileUXUINew() {
+  return (
+    <Layout>
+      <h2 style={{ marginTop: '50px' }}>Mobile UX/UI</h2>
+      <h5 style={{ marginBottom: "50px" }}>2020</h5>
+
+      <p>Work with a small startup team to create simple branding and a prototype for their food waste app.</p>
+
+      <div style={{ textAlign: 'center' }}>
+        <img src="https://oliviavoler.com/wp-content/uploads/2020/10/phone-with-groceries-smaller.png" 
+            alt="" width="800"/>
+        <img src="https://oliviavoler.com/wp-content/uploads/2020/10/flat-lay-mockup@2x.png" 
+            alt="" width="800" />
+      </div>
+
+      <h5 className="portfolioSectionTitle">Project</h5>
+
+      <p>Create a and simple branding for the app.</p>
+
+      <p>Based off of mockups and a test version of the app, create high-fidelity mockups and prototype them, then make the handoff to the developers.</p>
+
+      <p>Provide guidance and feedback on initial versions of the app to ensure that they are following the standards of the mockups.</p>
+
+      <h5 className="portfolioSectionTitle">Existing materials</h5>
+
+      <p>
+      The app had already been primitively started when I joined, 
+      but the team wanted to restart completely with a new coding language and new UI, 
+      so that’s where I was brought in. 
+      Here are a few screenshots of the UI before I started: the first two are the very original mockups,
+      and the second two are work they were doing just as I began.
+      </p>
+
+      <img src="https://res.cloudinary.com/ofvoler/image/upload/v1616990457/fresh-existing-materials.png" alt="" />
+
+      <h5 className="portfolioSectionTitle">Branding</h5>
+
+      <p>
+      I started the branding with a logo. The team had originally chosen a logo with a squirrel, 
+      which doesn’t exactly go hand in hand with preventing food waste. 
+      I started out by sketching logos that reminded me of something fresh—as a result, 
+      I ended up with lemon-themed logos. I then translated a couple of these ideas into digital logos.
+      </p>
+
+      <img src="https://res.cloudinary.com/ofvoler/image/upload/v1617653528/fresh-lemon-logos.png" />
+
+      <p>
+      Even though the team really liked these logos (especially the ones on the left), 
+      the lemon theme that I landed on definitely reminded us of a certain food delivery company. 
+      We didn’t want to run into any sort of copyright problems, so I thought of ways to redesign the logo with different fruits:
+      </p>
+
+    <img />
+
+    </Layout>
+  )
+}
