26e84693f48a64ab95f95086a8cabd76c504ca2a
ofvoler <ofvoler@gmail.com>
2021-04-05 22:24:38 -0400

    Create component to format portfolio page images

---
+++ src/components/portfolioPageImage.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function PortfolioPageImage({ children }) {
+    return (
+        <div style={{ maxWidth: "800px", textAlign: "center", margin: "0 auto" }}>
+            {children}
+        </div>
+    )
+}
